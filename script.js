7f2187f103fdcb1062f7bccdc820eb5106b05b89
Hoang Phuc <49684127+Huangphoux@users.noreply.github.com>
2025-08-07 15:18:19 +0700

    Game functioning without win condition

--- script.js
+++ script.js
@@ -1,25 +1,102 @@
-let hamster = {
-    stomach: [],
+const gameBoard = (function () {
+    const rows = 3;
+    const columns = 3;
+    const board = [];
 
-    eat(food) {
-        // this.stomach.push(food);
-        this.stomach = [food]
-    },
-};
+    for (let i = 0; i < rows; i++) {
+        board[i] = [];
+        for (let j = 0; j < columns; j++) {
+            board[i].push(Cell());
+        }
+    }
 
-let speedy = {
-    __proto__: hamster,
-    // stomach: [],
-};
+    const getBoard = () => board;
 
-let lazy = {
-    __proto__: hamster,
-    // stomach: [],
-};
+    const printBoard = () => {
+        const boardWithCellValues = board.map((row) => row.map((cell) => cell.getValue()));
 
-// This one found the food
-speedy.eat("apple");
-console.log(speedy.stomach); // apple
+        console.log(boardWithCellValues);
+    };
 
-// This one also has it, why? fix please.
-console.log(lazy.stomach); // apple
+    const placeToken = (row, column, player) => {
+        if (board[row][column].getValue() !== 0) return;
+
+        board[row][column].addToken(player);
+    };
+
+    return { getBoard, placeToken, printBoard };
+})();
+
+// 0: empty
+// 1: X
+// 2: O
+
+function Cell() {
+    let value = 0;
+
+    const addToken = (player) => {
+        value = player;
+    };
+
+    const getValue = () => value;
+
+    return { addToken, getValue };
+}
+
+function createPlayer(name, token) {
+    return { name, token };
+}
+
+const gameController = (function (playerOneName = "Player One", playerTwoName = "Player Two") {
+    const players = [createPlayer(playerOneName, 1), createPlayer(playerTwoName, 2)];
+
+    let activePlayer = players[0];
+
+    const switchPlayerTurn = () => {
+        activePlayer = activePlayer === players[0] ? players[1] : players[0];
+    };
+
+    const getActivePlayer = () => activePlayer;
+
+    const printNewRound = () => {
+        gameBoard.printBoard();
+        console.log(`${getActivePlayer().name}'s turn.`);
+    };
+
+    const checkForWinner = () => {
+        // row
+        for (let i = 0; i < rows; i++) {
+            for (let j = 0; j < columns; j++) {
+                board[i][j].getValue();
+            }
+        }
+
+        // column
+
+        // diagonal
+
+        // tie
+    };
+
+    const playRound = (row, column) => {
+        console.log(`Placing ${getActivePlayer().name}'s token into row ${row}, column ${column}...`);
+        gameBoard.placeToken(row, column, getActivePlayer().token);
+
+        checkForWinner();
+
+        switchPlayerTurn();
+        printNewRound();
+    };
+
+    printNewRound();
+
+    return { playRound, getActivePlayer };
+})();
+
+gameController.playRound(0, 0);
+gameController.playRound(0, 1);
+gameController.playRound(0, 2);
+gameController.playRound(1, 2);
+gameController.playRound(1, 1);
+gameController.playRound(2, 0);
+gameController.playRound(2, 2);
